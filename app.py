eaabc5049d558d80f47c8a821d06893163eea865
M-Aimar <90445954+M-Aimar@users.noreply.github.com>
2023-12-02 16:01:46 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as app
+import numpy as np
+from PIL import ImageOps
+import tensorflow as tf
+
+# Model loading
+trained_model = tf.keras.models.load_model('model.h5')
+
+# Image preprocessing
+def prepare_image(file):
+    image = ImageOps.open(file)
+    resized_image = image.resize((224, 224))
+    image_to_array = np.array(resized_image) / 255.0
+    expanded_array = np.expand_dims(image_to_array, axis=0)
+    return expanded_array
+
+# Defect detection in metal
+def detect_defect(image_file):
+    try:
+        prepared_img = prepare_image(image_file)
+        defect_prediction = trained_model.predict(prepared_img)
+        return defect_prediction
+    except Exception as error:
+        app.error("Invalid Image. Please upload a relevant image.")
+        return np.array([[1.0]])
+
+def run_app():
+    app.title("Metal Defect Detection Tool")
+
+    user_uploaded_file = app.file_uploader("Choose an image to check for defects", type=["jpg", "jpeg", "png"])
+
+    if user_uploaded_file is not None:
+        image_file_path = f"uploads/{user_uploaded_file.name}"
+        with open(image_file_path, "wb") as file:
+            file.write(user_uploaded_file.getbuffer())
+
+        app.image(user_uploaded_file, caption="Your Uploaded Image", use_column_width=True)
+
+        if app.button("Analyze Image"):
+            defect_result = detect_defect(image_file_path)
+            outcome = "Defective" if defect_result[0][0] >= 0.5 else "Normal"
+            app.write(f"Analysis Outcome: {outcome} (Value: {defect_result[0][0]:.2f})")
+
+if __name__ == "__main__":
+    run_app()
